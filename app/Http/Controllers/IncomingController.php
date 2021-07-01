c04fea354274343001997a414fd5edda0a4a64c4
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2021-07-01 17:59:36 +0800

    remove checking from incoming

--- app/Http/Controllers/IncomingController.php
+++ app/Http/Controllers/IncomingController.php
@@ -17,49 +17,51 @@ class IncomingController extends Controller
      */
     public function store(Request $request, $botid) {
 
-        $validator = Validator::make(
-            $request->all(), [ 
-                'callback_query' => 'prohibited_unless:message,null|array',
-                'callback_query.message.chat.id'  => 'prohibited_unless:message,null|integer',
-                'callback_query.message.from.username'  => 'prohibited_unless:message,null|string',
-                'callback_query.message.data'  => 'prohibited_unless:message,null|string',
+        // $validator = Validator::make(
+        //     $request->all(), [ 
+        //         'callback_query' => 'prohibited_unless:message,null|array',
+        //         'callback_query.message.chat.id'  => 'prohibited_unless:message,null|integer',
+        //         'callback_query.message.from.username'  => 'prohibited_unless:message,null|string',
+        //         'callback_query.message.data'  => 'prohibited_unless:message,null|string',
         
-                'message' => 'prohibited_unless:callback_query,null|array',
-                'message.chat.id'  => 'prohibited_unless:callback_query,null|integer',
-                'message.from.username'  => 'prohibited_unless:callback_query,null|string',
-                'message.data'  => 'prohibited_unless:callback_query,null|string',
-            ]
-        );
+        //         'message' => 'prohibited_unless:callback_query,null|array',
+        //         'message.chat.id'  => 'prohibited_unless:callback_query,null|integer',
+        //         'message.from.username'  => 'prohibited_unless:callback_query,null|string',
+        //         'message.data'  => 'prohibited_unless:callback_query,null|string',
+        //     ]
+        // );
 
-        // capture only if both fails
-        if ($validator->fails()) {
-            return response()->json(
-                [
-                    'status' => false,
-                    'errors' => $validator->errors(),
-                ],
-                400
-            );
-        }
+        // // capture only if both fails
+        // if ($validator->fails()) {
+        //     return response()->json(
+        //         [
+        //             'status' => false,
+        //             'errors' => $validator->errors(),
+        //         ],
+        //         400
+        //     );
+        // }
 
-        // receive from telegram
-        if (isset($request['callback_query'])){
-            $userId = $request["callback_query"]["message"]["chat"]["id"];
-            $username = "@".$request["callback_query"]["message"]["from"]["username"]; // will be use as reference id
-            $message = $request["callback_query"]["data"];
-        } else if (isset($request['message'])) {
-            $userId = $request["message"]["chat"]["id"];
-            $username = "@".$request["message"]["from"]["username"]; // will be use as reference id
-            $message = $request["message"]["text"];
-        } else {
-            return response()->json(
-                [
-                    'status' => false,
-                    'errors' => "Either callback query field or message field can't be null",
-                ],
-                400
-            );
-        }
+        // // receive from telegram
+        // if (isset($request['callback_query'])){
+        //     $userId = $request["callback_query"]["message"]["chat"]["id"];
+        //     $username = "@".$request["callback_query"]["message"]["from"]["username"]; // will be use as reference id
+        //     $message = $request["callback_query"]["data"];
+        // } else if (isset($request['message'])) {
+        //     $userId = $request["message"]["chat"]["id"];
+        //     $username = "@".$request["message"]["from"]["username"]; // will be use as reference id
+        //     $message = $request["message"]["text"];
+        // } else {
+        //     return response()->json(
+        //         [
+        //             'status' => false,
+        //             'errors' => "Either callback query field or message field can't be null",
+        //         ],
+        //         400
+        //     );
+        // }
+
+        
         \Log::channel('transaction')->info("LOG Start Incoming ------------------------------------------------");
         \Log::channel('transaction')->info("Telegram -> PATH " . config('app.url') . preg_replace('/[\r\n\t ]+/','',$request->getRequestUri()));
         \Log::channel('transaction')->info("Telegram -> HEADER", $request->header());
